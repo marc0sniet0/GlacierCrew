2d3cdba0b99c05ad09f232665f159572018de240
marc0sniet0 <marcos.nieto.iglesias@outlook.es>
2025-10-30 15:26:58 +0100

    pagina entradas añadida

--- js/script.js
+++ js/script.js
@@ -61,24 +61,6 @@ cuentaAtrasFecha.setSeconds(cuentaAtrasFecha.getSeconds() + 34);
 
 
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const cuentaContainer = document.querySelector('.cuenta-container');
-//   let lastScrollTop = 0;
-
-//   window.addEventListener('scroll', () => {
-//     const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-//     if (currentScroll > lastScrollTop && currentScroll > 100) {
-    
-//       cuentaContainer.classList.add('hide');
-//     } else if (currentScroll < lastScrollTop) {
-      
-//       cuentaContainer.classList.remove('hide');
-//     }
-
-//     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; 
-//   });
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
   const cuentaContainer = document.querySelector('.cuenta-container');
@@ -99,6 +81,44 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+
+// COMPRAR ENTRADAS //
+
+document.addEventListener("DOMContentLoaded", function () {
+    const inputs = document.querySelectorAll('input[type="number"]');
+    const totalPriceElement = document.getElementById("totalPrice");
+
+    // Añade a cada input un evento change para calcular el precio total
+    inputs.forEach((input) => {
+        input.addEventListener("change", calculateTotalPrice);
+    });
+
+    // Función para calcular el precio total
+    function calculateTotalPrice() {
+        let totalPrice = 0;
+        inputs.forEach((input) => {
+            const price = parseFloat(input.dataset.price) || 0;
+            const quantity = parseInt(input.value) || 0;
+            totalPrice += price * quantity;
+
+            // Si el precio es mayor que 0, se habilita el botón de comprar
+            if (totalPrice > 0) {
+                document.getElementById("buyFood").classList.remove("disabled");
+            } else {
+                document.getElementById("buyFood").classList.add("disabled");
+            }
+        });
+        totalPriceElement.textContent = totalPrice.toFixed(2) + " €";
+    }
+
+    // La primera llamada a la función hará el calculo inicial para poner 0.00 €
+    calculateTotalPrice();
+
+    // Evitar el envio del formulario si el usuario pulsa enter o el precio es 0
+    document.getElementById("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+    });
+});
 
 
 
